30b218f23bc42fa33e6c8221d75dfd0ae726dd90
Tim Scully <hello@timscully.co.uk>
2020-05-13 14:58:45 +0100

    return 3rd output to add previous two elements in array

--- fibonacci.js
+++ fibonacci.js
@@ -8,9 +8,13 @@ function fibonacciGenerator(n) {
       output = [0];
     } else if (n === 2) {
       output = [0, 1];
+    } else {
+      output = [0, 1];
+      output.push(output[0] + output[1]);
     }
 
-    return output;
+    // return output;
+    console.log(output);
     //Return an array of fibonacci numbers starting from 0.
 
 //Do NOT change any of the code below 👇
