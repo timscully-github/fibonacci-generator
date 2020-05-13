58b91d7e45200338e483d5620677a00c5d1cc946
Tim Scully <hello@timscully.co.uk>
2020-05-13 14:56:05 +0100

    return output as 0 if n is 1

---
+++ fibonacci.js
@@ -0,0 +1,17 @@
+function fibonacciGenerator(n) {
+//Do NOT change any of the code above 👆
+
+    //Write your code here:
+
+    var output = [];
+    if (n === 1) {
+      output = [0];
+    }
+
+    return output;
+    //Return an array of fibonacci numbers starting from 0.
+
+//Do NOT change any of the code below 👇
+}
+
+fibonacciGenerator(5);
